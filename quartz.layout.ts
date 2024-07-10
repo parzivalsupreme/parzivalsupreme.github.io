c432693c8ee7be178be12481b68440432ea20ea9
Elijah Cezar <ajah35961@gmail.com>
2024-07-10 22:09:39 +0800

    Quartz sync: Jul 10, 2024, 10:09 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,11 +24,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.MobileOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
